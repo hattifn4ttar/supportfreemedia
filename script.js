af2fb15171a7dbea109f52349cf048c662a7c989
hattifn4ttar <hattifnatt4r@gmail.com>
2022-03-16 20:34:27 -0400

    ru version

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 document.getElementById('githubLink').addEventListener('click', () => window.open('https://github.com/hattifn4ttar/youtube_promoteplaylist'));
 document.getElementById('youtubeLink').addEventListener('click', () => window.open('https://www.youtube.com/watch?v=eTSipyTLSjo'));
+
+document.getElementById('ruLink').addEventListener('click', () => location.replace('ru.html'));
+document.getElementById('enLink').addEventListener('click', () => location.replace('./'));
